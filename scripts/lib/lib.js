65590af5900469372181ea06eefaec0c38d68398
Nicolas Grobelny <miki4920@gmail.com>
2021-06-01 15:47:32 +0100

    Added all abilities as well as strikes and hp handling

--- scripts/lib/lib.js
+++ scripts/lib/lib.js
@@ -1,9 +1,4 @@
 import {ApplyData} from "./apply_data.js"
-<<<<<<< Updated upstream
-import {abilities} from "./data/abilities.js"
-
-
-=======
 import {data} from "./data/abilities.js"
 
 
@@ -15,7 +10,6 @@ function upper_case(string) {
     return string.replace(/\w\S*/g, (w) => (w.replace(/^\w/, (c) => c.toUpperCase())));
 }
 
->>>>>>> Stashed changes
 export function creature_builder_button(sheet, html) {
     const actor = sheet.actor;
     if (!(actor.data.type === 'npc' && actor.can(game.user, 'update'))) {
@@ -26,22 +20,9 @@ export function creature_builder_button(sheet, html) {
         return;
     }
     let button = $(`<a class="popout" style><i class="fas fa-book"></i>Monster Maker</a>`);
-<<<<<<< Updated upstream
-    let template_abilities = Object.keys(abilities["abilities"]);
-    button.on('click', async () => {
-        new Dialog({
-            content: await renderTemplate("modules/foundryvtt-pf2e-monster-maker/templates/form.html", {"template_abilities": template_abilities}),
-            buttons: {
-                submit: { label: "Submit", callback: (html) => {
-                        const results = new FormData(html.find("form")[0]);
-                        const data_applier = new ApplyData(actor, results);
-                        data_applier.apply_data();
-                    }}
-=======
     let template_keys = data["keys"]
     Handlebars.registerHelper('equals', equals);
     Handlebars.registerHelper('upper_case', upper_case)
-    console.log(actor);
     button.on('click', async () => {
         new Dialog({
             content: await renderTemplate("modules/foundryvtt-pf2e-monster-maker/templates/form.html",
@@ -56,7 +37,6 @@ export function creature_builder_button(sheet, html) {
                         data_applier.apply_data();
                     }
                 }
->>>>>>> Stashed changes
             }
         }).render(true);
     });
