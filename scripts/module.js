bda211e131bbd017c67610663ca9f8805c6bffeb
miki4920 <miki4920@gmail.com>
2022-04-20 20:06:49 +0100

    Implemented clipboard pasting

--- scripts/module.js
+++ scripts/module.js
@@ -1,4 +1,48 @@
-import {creature_builder_button, register_settings} from "./lib/lib.js"
+import {creature_builder_button, handle_token_drop, handle_token_clipboard} from "./lib/lib.js"
 
-Hooks.on('ready', register_settings)
+Hooks.on("init", async function() {
+    await game.keybindings.register("foundryvtt-pf2e-monster-maker", "clipboard", {
+        name: "Paste Image from Clipboard",
+        restricted: true,
+        uneditable: [
+            {key: "KeyV", modifiers: [ KeyboardManager.MODIFIER_KEYS.CONTROL ]}
+        ],
+        onDown: () => {
+            handle_token_clipboard()
+        },
+    });
+
+})
+
+Hooks.on('ready', async function() {
+    await new DragDrop({
+        callbacks: {
+            drop: handle_token_drop
+        }
+    }).bind(document.getElementById("board"));
+
+    await game.settings.register("foundryvtt-token-maker", "tokenDirectory", {
+        name: "Download Directory",
+        hint: "Set a Directory for tokens",
+        scope: "world",
+        config: true,
+        type: String,
+        filePicker: "folder",
+        default: "",
+    });
+    await game.settings.register("foundryvtt-pf2e-monster-maker", "roadmaps", {
+        scope: 'world',
+        config: false,
+        type: Object,
+        default: {}
+    });
+    await game.settings.register("foundryvtt-pf2e-monster-maker", "traits", {
+        scope: 'client',
+        config: false,
+        type: Object,
+        default: {}
+    });
+})
+
 Hooks.on('renderActorSheet', creature_builder_button);
+
