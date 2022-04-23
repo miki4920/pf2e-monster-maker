aa237187672bdad5629b23447d2e7dd44765dfcf
miki4920 <miki4920@gmail.com>
2022-04-23 23:29:03 +0100

    Added an ability to replace tokens

--- scripts/lib/lib.js
+++ scripts/lib/lib.js
@@ -13,7 +13,7 @@ async function create_dialog(actor) {
     let template_keys = data["keys"];
     let roadmaps = Roadmap.getItems()
     let dialog = new Dialog({
-        content: await renderTemplate("modules/foundryvtt-pf2e-monster-maker/templates/form.html",
+        content: await renderTemplate("modules/foundryvtt-pf2e-monster-maker/templates/monster_maker.html",
             {
                 "template_keys": template_keys,
                 "roadmaps": roadmaps
@@ -57,20 +57,6 @@ export function creature_builder_button(sheet, html) {
     }
 }
 
-export async function handle_token_drop(event) {
-    if (event.dataTransfer.items) {
-        for (let i = 0; i < event.dataTransfer.items.length; i++) {
-            if (event.dataTransfer.items[i].kind === 'file') {
-                event.preventDefault();
-                await create_actor_from_file(event.dataTransfer.items[i].getAsFile())
-            } else {
-                await canvas._dragDrop.callbacks.drop(event);
-            }
-        }
-
-    }
-}
-
 function make_id() {
     let result = '';
     let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -82,24 +68,7 @@ function make_id() {
     return result;
 }
 
-export async function handle_token_clipboard() {
-    let clip_items = await navigator.clipboard.read();
-    for (let idx = 0; idx < clip_items[0].types.length; idx++) {
-        const ftype = clip_items[0].types[idx];
-        if (ftype.startsWith("image/")) {
-            let blob = await clip_items[0].getType(ftype)
-            let file = await new File([blob], make_id() + ".png")
-            await create_actor_from_file(file);
-        }
-    }
-
-
-}
-
-async function create_actor_from_file(file) {
-    const file_path = game.settings.get("foundryvtt-token-maker", "tokenDirectory");
-    let path = file_path + "/" + file.name
-    await FilePicker.upload("data", file_path, file);
+async function create_actor_token(path) {
     let actor = await Actor.create(
         {
             name: "Rename Me!",
@@ -110,54 +79,35 @@ async function create_actor_from_file(file) {
     await create_dialog(actor);
 }
 
-async function replace_token(actor) {
-    new Dialog({
-        title: 'Example Dialog',
-        content: `
-    <form>
-      <div class="form-group">
-        <label>Image Url</label>
-        <input type='text' name='url_input_field'/>
-      </div>
-    </form>`,
-        buttons: {
-            yes: {
-                icon: "<i class='fas fa-check'></i>",
-                label: `Apply Changes`,
-            }
-        },
-        close: html => {
-            let result = html.find('input[name=\'url_input_field\']');
-            result = result.val();
-            if (result !== "") {
-                fetch(result, {
-                    method: "GET",
-                    headers: {
-                        "x-requested-with": "foundry"
-                    },
-                }).then(res => res.blob())
-                    .then(async blob => {
-                        let file = await new File([blob], make_id() + ".png")
-                        const file_path = game.settings.get("foundryvtt-token-maker", "tokenDirectory");
-                        let path = file_path + "/" + file.name
-                        await FilePicker.upload("data", file_path, file);
-                        await actor.update({"img": path})
-                        await actor.update({"token.img": path})
-
-                    });
-            }
-        }
-    }).render(true);
+async function change_actor_token(path, actor) {
+    await actor.update({"img": path})
+    await actor.update({"token.img": path})
 }
 
-export function replace_token_button(sheet, html) {
-    const actor = sheet.actor;
-    let element = html.find('.window-header .window-title');
-    if (check_sheet(actor, element)) {
-        let button = $(`<a class="popout" style><i class="fas fa-book"></i>Replace Token</a>`);
-        button.on('click', function () {
-            replace_token(actor)
-        });
-        element.after(button);
+export async function handle_token_clipboard(event, sheet) {
+    if(sheet && !document.getElementById(sheet.options.id)) {
+        await handle_token_clipboard(event, null)
+        document.onpaste = async function(event){
+            await handle_token_clipboard(event, null)
+        }
     }
+    else {
+        let items = (event.clipboardData || event.originalEvent.clipboardData).items;
+        for (let i=0; i < items.length; i++) {
+            let item = items[i];
+            if (item.kind === 'file') {
+                let file = await new File([item.getAsFile()], make_id() + ".png");
+                const file_path = game.settings.get("foundryvtt-token-maker", "tokenDirectory");
+                await FilePicker.upload("data", file_path, file);
+                let path = file_path + "/" + file.name
+                if (!sheet) {
+                    await create_actor_token(path)
+                }
+                else {
+                    await change_actor_token(path, sheet.actor)
+                }
+            }
+        }
+    }
+
 }
