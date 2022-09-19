c5ab6cca38e31557186ddd95e9b10cfe93e29044
miki4920 <miki4920@gmail.com>
2022-09-19 18:09:35 +0100

    Implemented webpack and typescript

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+Hooks.on('ready', async function () {
+    await game["settings"].register("foundryvtt-pf2e-monster-maker", "roadmaps", {
+        scope: 'world',
+        config: false,
+        type: Object,
+        default: {}
+    });
+})
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.ts',
+    watch: true,
+    watchOptions: {
+        aggregateTimeout: 600,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+};
