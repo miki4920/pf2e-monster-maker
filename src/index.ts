30d5a265f2b70116cda6bd30c4644d1a694ea0c6
miki4920 <miki4920@gmail.com>
2022-09-20 14:55:50 +0100

    Added rendering for the form

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,25 @@
-Hooks.on('ready', async function () {
+import {MonsterMaker} from "./MonsterMaker"
+Hooks.on('init', async function () {
     await game["settings"].register("foundryvtt-pf2e-monster-maker", "roadmaps", {
         scope: 'world',
         config: false,
         type: Object,
         default: {}
     });
+})
+
+Hooks.on("renderActorSheet", async function (sheet, html) {
+    let actor = sheet.object
+    if (actor?.type !== "npc") {
+        return;
+    }
+    if(!actor.canUserModify(game["user"], "update")) {
+        return;
+    }
+    let element = html.find(".window-header .window-title");
+    let button = $(`<a class="popout" style><i class="fas fa-book"></i>Monster Maker</a>`);
+    button.on("click", () => {
+        new MonsterMaker(actor).render(true)
+    })
+    element.after(button);
 })
