bed92a5a1a78152b0386128e1ba720806b268d2d
miki4920 <miki4920@gmail.com>
2022-09-26 21:28:19 +0100

    Created functions that add non-attribute items, such as strikes

--- src/MonsterMaker.ts
+++ src/MonsterMaker.ts
@@ -1,9 +1,12 @@
-import {actorFields, DefaultCreatureStatistics, Levels} from "./Keys";
+import {actorFields, DefaultCreatureStatistics, Levels, Statistics, Skills, Options} from "./Keys";
 import {statisticValues} from "./Values";
-import Document from "@league-of-foundry-developers/foundry-vtt-types/src/foundry/common/abstract/document.mjs";
+import {BaseActor} from "@league-of-foundry-developers/foundry-vtt-types/src/foundry/common/documents.mjs";
+
 
 export class MonsterMaker extends FormApplication {
     data = DefaultCreatureStatistics
+    actor = <BaseActor>this.object
+    level = "-1"
     override activateListeners(): void {
         for(const category of this.data) {
             for(const statistic of category.statisticEntries) {
@@ -27,19 +30,103 @@ export class MonsterMaker extends FormApplication {
         });
     }
 
-    // @ts-ignore
-    protected async _updateObject(event: Event, formData?: object): Promise<unknown> {
+    applyName(formData) {
+        let name = formData[Statistics.name] ? formData[Statistics.name] : "Placeholder"
+        return {"name": name}
+    }
+
+    applyLevel() {
+        return {"system.details.level.value": parseInt(this.level)}
+    }
+
+    applyHitPoints(formData) {
+        let option = formData[Statistics.hp]
+        let hitPoints = parseInt(statisticValues[Statistics.hp][this.level][option])
+        return {"system.attributes.hp.value": hitPoints}
+    }
+
+    applyStrike(formData) {
+        const strikeBonusOption = formData[Statistics.strikeBonus]
+        const strikeDamageOption = formData[Statistics.strikeDamage]
+        const strikeBonus = parseInt(statisticValues[Statistics.strikeBonus][this.level][strikeBonusOption])
+        const strikeDamage = statisticValues[Statistics.strikeDamage][this.level][strikeDamageOption]
+        let strike = {
+            name: 'Strike',
+            type: 'melee',
+            data: {
+                damageRolls: [
+                    {
+                        damage: strikeDamage,
+                    },
+                ],
+                bonus: {
+                    value: strikeBonus,
+                },
+            },
+        };
+        return Item.create(strike, {parent: this.actor})
+    }
+
+    applySpellcasting(formData) {
+        const spellcastingOption = formData[Statistics.spellcasting]
+        const spellcastingBonus = parseInt(statisticValues[Statistics.spellcasting][this.level][spellcastingOption])
+        const spellcasting = {
+            name: "Spellcasting",
+            type: "spellcastingEntry",
+            system: {
+                spelldc: {
+                    value: spellcastingBonus,
+                    dc: spellcastingBonus+8,
+                },
+                tradition: {
+                    value: 'arcane',
+                },
+                prepared: {
+                    value: 'innate',
+                },
+                showUnpreparedSpells: { value: true },
+            }
+        };
+        return Item.create(spellcasting, {parent: this.actor})
+    }
+
+    async applySkills(formData) {
+        for(const skillName of Skills) {
+            const option = formData[skillName]
+            if (option !== Options.none) {
+                const value = parseInt(statisticValues[skillName][this.level][option])
+                const skill = {
+                    name: skillName,
+                    type: 'lore',
+                    data: {
+                        mod: {
+                            value: value,
+                        },
+                    },
+                };
+                await Item.create(skill, {parent: this.actor})
+            }
+        }
+    }
+
+    protected async _updateObject(event: Event, formData?: object) {
         if(formData) {
             let updateData = {}
-            let level = formData["Level"]
+            this.level = formData[Statistics.level]
             for(const key of Object.keys(formData)) {
                 if(actorFields[key]) {
                     let actorField = actorFields[key]
                     let option = formData[key]
-                    updateData[actorField] = parseInt(statisticValues[key][level][option])
+                    updateData[actorField] = parseInt(statisticValues[key][this.level][option])
                 }
             }
-            await (<Document<any>>this.object).update(updateData);
+            Object.assign(updateData, this.applyName(formData))
+            Object.assign(updateData, this.applyLevel())
+            await this.actor.update(updateData);
+            await this.actor.update(this.applyHitPoints(formData))
+            await this.applyStrike(formData)
+            await this.applySpellcasting(formData)
+            await this.applySkills(formData)
         }
     }
 
